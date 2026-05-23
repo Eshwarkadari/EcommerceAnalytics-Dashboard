ca0901cf7a043e5aa36b85b873699ad972aaa1ae
KADARI ESHWAR <eshwarkadari78@gmail.com>
2026-05-24 01:41:04 +0530

    Add file

---
+++ app.py
@@ -0,0 +1,42 @@
+"""
+ECommerce Analytics Dashboard — Main Flask App
+Author: Kadari Eshwar | B.Tech ECE, JNTU Hyderabad
+"""
+
+from flask import Flask, render_template, jsonify
+from database import init_db, get_kpis, get_monthly_revenue, get_category_sales, get_top_products, get_recent_orders, get_region_sales
+
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+    kpis = get_kpis()
+    return render_template("dashboard.html", kpis=kpis)
+
+@app.route("/api/monthly-revenue")
+def monthly_revenue():
+    return jsonify(get_monthly_revenue())
+
+@app.route("/api/category-sales")
+def category_sales():
+    return jsonify(get_category_sales())
+
+@app.route("/api/top-products")
+def top_products():
+    return jsonify(get_top_products())
+
+@app.route("/api/recent-orders")
+def recent_orders():
+    return jsonify(get_recent_orders())
+
+@app.route("/api/region-sales")
+def region_sales():
+    return jsonify(get_region_sales())
+
+@app.route("/api/kpis")
+def kpis():
+    return jsonify(get_kpis())
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
